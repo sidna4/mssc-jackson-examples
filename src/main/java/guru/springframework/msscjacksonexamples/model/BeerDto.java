44333867e2d30dc2c405ecd2a4ab9fb79998e64a
Sidney Antommarchi <sidna4@yahoo.com>
2021-01-20 16:08:37 -0500

    Lesson 110

--- src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
+++ src/main/java/guru/springframework/msscjacksonexamples/model/BeerDto.java
@@ -1,5 +1,6 @@
 package guru.springframework.msscjacksonexamples.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,7 +36,10 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
 }
